adc548a4cd75556b676444475b7a672947be9d9a
Aiden Vigue <aiden@vigue.me>
2023-05-21 21:23:35 -0400

    basic playlist actions!

--- src/components/PatternGridItemPlaceholder.vue
+++ src/components/PatternGridItemPlaceholder.vue
@@ -1,6 +1,6 @@
 <template>
   <div :style="style" class="flex flex-col items-center m-4 p-2">
-    <div class="h-44 w-44 rounded-full border-[3px]"></div>
+    <div class="h-36 w-36 md:h-44 md:w-44 rounded-full border-[3px]"></div>
     <span class="w-[90%] text-md font-medium">...</span>
   </div>
 </template>
--- src/components/PlaylistModal.vue
+++ src/components/PlaylistModal.vue
@@ -1,23 +1,93 @@
 <script setup lang="ts">
 import type { Playlist } from '@/stores/files'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import { VueFinalModal } from 'vue-final-modal'
 import useFilesStore from '../stores/files'
-import { XMarkIcon } from '@heroicons/vue/24/outline'
+import {
+  XMarkIcon,
+  PlayIcon,
+  PencilIcon,
+  TrashIcon,
+  ArrowDownTrayIcon
+} from '@heroicons/vue/24/outline'
 import PatternPreview from './PatternPreview.vue'
 import DeleteConfirmationModal from './DeleteConfirmationModal.vue'
+import draggable from 'vuedraggable'
+import { useToast } from 'vue-toast-notification'
+import { useModal } from 'vue-final-modal'
+import useTableStatusStore from '@/stores/tableStatus'
 
 interface PlaylistModalProps {
   playlist: Playlist
 }
 
+const table = useTableStatusStore()
 const files = useFilesStore()
+const toast = useToast()
 
 const props = defineProps<PlaylistModalProps>()
 
 const isPlaylistDownloaded = computed(() => {
   return files.playlists.find((playlist) => playlist.uuid === props.playlist.uuid) !== undefined
 })
+
+const isCurrentlyPlayingThisPlaylist = computed(() => {
+  return table.currentPlaylistID === props.playlist.uuid
+})
+
+const downloadThisPlaylist = async function () {
+  isPerformingAction.value = true
+  try {
+    await files.downloadPlaylist(props.playlist)
+    toast.success(`Downloaded ${props.playlist.name}`)
+  } catch (e) {
+    toast.error('Error downloading playlist!')
+  }
+  isPerformingAction.value = false
+}
+
+const playThisPlaylist = async function () {
+  isPerformingAction.value = true
+  try {
+    await table.playFile(`${props.playlist.uuid}.seq`)
+    toast.success(`Playling ${props.playlist.name}`)
+  } catch (e) {
+    toast.error('Error playing playlist!')
+  }
+  isPerformingAction.value = false
+}
+
+const deleteThisPlaylist = async function () {
+  isPerformingAction.value = true
+
+  const { open, close } = useModal({
+    component: DeleteConfirmationModal,
+    attrs: {
+      itemName: props.playlist.name,
+      onClose() {
+        close()
+        isPerformingAction.value = false
+      },
+      onConfirm() {
+        close()
+        files
+          .deleteFile(`${props.playlist.uuid}.seq`)
+          .catch(() => {
+            toast.error('Error deleting playlist!')
+          })
+          .finally(async () => {
+            files.playlists = files.playlists.filter((playlist) => playlist.uuid !== props.playlist.uuid)
+            await files.saveManifest()
+            toast.success(`Deleted ${props.playlist.name}`)
+            isPerformingAction.value = false
+          })
+      }
+    }
+  })
+  await open()
+}
+
+const isPerformingAction = ref(false)
 
 const emit = defineEmits<{
   (e: 'close'): void
@@ -32,7 +102,7 @@ const emit = defineEmits<{
   >
     <div class="flex flex-col justify-between h-full pb-20">
       <div class="flex justify-between">
-        <div class="grow-1">
+        <div class="flex-1">
           <button @click="emit('close')" class="hover:scale-[1.2] transform-gpu duration-300">
             <XMarkIcon class="w-7 h-7" />
           </button>
@@ -42,51 +112,55 @@ const emit = defineEmits<{
             playlist.name
           }}</span>
         </div>
-        <div class="grow-1" />
+        <div class="flex-1" />
       </div>
       <div class="flex items-center flex-col align-center w-full gap-3">
         <PatternPreview
-          :pattern="{uuid: playlist.featured_pattern, name: '', date: ''}"
+          :pattern="{ uuid: playlist.featured_pattern, name: '', date: '' }"
           class="md:h-80 md:w-80 w-[60vw] h-[60vw] rounded-full border-gray-500 border-[3px] bg-gray-800 group-hover:scale-105 transition transform-gpu duration-300"
           lineColor="#ffffff"
           :showBall="false"
         />
-        <span class="text-md font-medium text-gray-300">{{ playlist.patterns.length }} patterns</span>
+        <span class="text-md font-medium text-gray-300"
+          >{{ playlist.patterns.length }} patterns</span
+        >
+        <div class="flex items-center flex-col align-center w-full gap-3 mt-6">
+          <div class="flex justify-evenly w-full">
+            <button
+              v-if="isPlaylistDownloaded"
+              :disabled="isCurrentlyPlayingThisPlaylist || isPerformingAction"
+              @click="playThisPlaylist"
+              class="hover:scale-[1.2] transform-gpu duration-300 disabled:scale-100 disabled:text-gray-500"
+            >
+              <PlayIcon class="w-7 h-7" />
+            </button>
+            <button
+              v-if="isPlaylistDownloaded"
+              :disabled="isPerformingAction"
+              @click="null"
+              class="hover:scale-[1.2] transform-gpu duration-300 disabled:scale-100 disabled:text-gray-500"
+            >
+              <PencilIcon class="w-7 h-7" />
+            </button>
+            <button
+              v-if="isPlaylistDownloaded"
+              :disabled="isPerformingAction"
+              @click="deleteThisPlaylist"
+              class="hover:scale-[1.2] transform-gpu duration-300 disabled:scale-100 disabled:text-gray-500"
+            >
+              <TrashIcon class="w-7 h-7" />
+            </button>
+            <button
+              v-if="!isPlaylistDownloaded"
+              :disabled="isPerformingAction"
+              @click="downloadThisPlaylist"
+              class="hover:scale-[1.2] transform-gpu duration-300 disabled:scale-100 disabled:text-gray-500"
+            >
+              <ArrowDownTrayIcon class="w-7 h-7" />
+            </button>
+          </div>
+        </div>
       </div>
-      <!--
-      <div class="flex justify-evenly" v-if="isPatternDownloaded">
-        <button
-          :disabled="isCurrentlyPlayingThisPattern || isPerformingAction"
-          @click="playThisPattern()"
-          class="hover:scale-[1.2] transform-gpu duration-300 disabled:scale-100 disabled:text-gray-500"
-        >
-          <PlayIcon class="w-7 h-7" />
-        </button>
-        <button
-          :disabled="true || isPerformingAction"
-          @click="null"
-          class="hover:scale-[1.2] transform-gpu duration-300 disabled:scale-100 disabled:text-gray-500"
-        >
-          <PlusIcon class="w-7 h-7" />
-        </button>
-        <button
-          :disabled="isPerformingAction"
-          @click="deleteThisPattern()"
-          class="hover:scale-[1.2] transform-gpu duration-300 disabled:scale-100 disabled:text-gray-500"
-        >
-          <TrashIcon class="w-7 h-7" />
-        </button>
-      </div>
-      <div class="flex justify-evenly" v-else>
-        <button
-          :disabled="isPerformingAction"
-          @click="downloadThisPattern"
-          class="hover:scale-[1.2] transform-gpu duration-300 disabled:scale-100 disabled:text-gray-500"
-        >
-          <ArrowDownTrayIcon class="w-7 h-7" />
-        </button>
-      </div>
-      -->
     </div>
   </VueFinalModal>
 </template>
